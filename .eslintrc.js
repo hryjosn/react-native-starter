668572758f2fd64542e94a4f2a12c64b89b44569
hryjosn <jo74705@gmail.com>
2023-03-25 13:20:14 +0800

    Add mobx and update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,59 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  env: {
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'prettier',
+    'standard',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'eslint-plugin-prettier',
+    'simple-import-sort',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  globals: {
+    React: true,
+    jsdom: true,
+    JSX: true,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    indent: 0,
+    camelcase: 0,
+    'comma-dangle': 0,
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
 };
--- src/container/App.tsx
+++ src/container/App.tsx
@@ -10,7 +10,7 @@ import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
+  Button,
   StyleSheet,
   Text,
   useColorScheme,
@@ -59,39 +59,20 @@ function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    height: '100%',
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+      <View style={{flex: 1}} />
+      <Button
+        onPress={() => {
+          console.log('clicked');
+        }}
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+        title="click me"
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
     </SafeAreaView>
   );
 }
